b7199c65855357597e308db163581c0eb6001266
Kate <cvetok_spb@bk.ru>
2022-10-06 19:10:45 +0300

    Задача-3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,20 +1,19 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
-Console.WriteLine("введите значение четверти"); // 1 2 3 4
+Console.WriteLine ("введите значение четверти"); // 1 2 3 4
 int a = int.Parse(Console.ReadLine());
-
-if (a=1)
+if (a == 1)
 {
-    Console.WriteLine ("Диапазон для возможных координат x>0 && y>0");
+    Console.WriteLine("Диапазон для возможных координат: x>0; y>0");
 }
-else if (a=2)
+else if (a == 2)
 {
-    Console.WriteLine ("Диапазон для возможных координат x<0 && y>0");
+    Console.WriteLine("Диапазон для возможных координат: x<0; y>0");
 }
-else if (a=3)
+else if (a == 3)
 {
-    Console.WriteLine ("Диапазон для возможных координат x<0 && y<0");
+    Console.WriteLine("Диапазон для возможных координат: x<0; y<0");
 }
-else if (a=4)
+else 
 {
-    Console.WriteLine ("Диапазон для возможных координат x>0 && y<0");
+    Console.WriteLine("Диапазон для возможных координат: x>0; y<0");
 }
